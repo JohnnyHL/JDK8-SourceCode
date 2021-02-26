7acc2d7bf29eaf91243df422274b5ba89b852819
JohnnyHL <316839314@qq.com>
2021-02-26 21:44:08 +0800

    JDK8源码环境

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author HL
+ * @date 2021/2/26 15:43
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
---
+++ test/com/hl/magic/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author HL
+ * @date 2021/2/26 17:16
+ */
+package com.hl.magic;
